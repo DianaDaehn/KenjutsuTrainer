90415940977d45e7b6129d20a3c33c39e4522e62
werner daehn <werner.daehn@googlemail.com>
2023-11-01 13:53:13 +0100

    update

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Logger : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class Logger : MonoBehaviour
     public Transform headSet;
     public LineRenderer leftTrail;
     public LineRenderer rightTrail;
-    public GameObject recordingText;
+    public Button recordingText;
     public int trailLength;
     private List<Frame> frames = new List<Frame>();
     private bool recording;
@@ -24,110 +25,136 @@ public class Logger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        recordingText.SetActive(false);
+        recording = false;
+    }
+
+    public void OnRecordButtonClick()
+    {
+        if (recording)
+        {
+            StopRecording();
+        }
+        else
+        {
+            StartRecording();
+        }
+    }
+
+    void StartRecording()
+    {
+        Debug.Log("Started recording");
+        recording = true;
+        var colors = recordingText.colors;
+        colors.normalColor = Color.red;
+        recordingText.colors = colors;
+        frames.Clear();
+        leftTrail.positionCount = 0;
+        rightTrail.positionCount = 0;
+        trailLengthCurrent = trailLength; // reset to the default traillength
+    }
+
+    void StopRecording()
+    {
+        Debug.Log("Stopped recording");
+        recording = false;
+        var colors = recordingText.colors;
+        colors.normalColor = Color.white;
+        recordingText.colors = colors;
+        playbackFrame = 10.0f;
+
+        using (var writer = new StreamWriter(File.OpenWrite(Application.persistentDataPath + "/log-" + DateTime.Now.ToString("s") + ".csv")))
+        {
+            writer.WriteLine("timestamp," +
+                "left_hand_x,left_hand_y,left_hand_z,left_hand_euler_x,left_hand_euler_y,left_hand_euler_z," +
+                "right_hand_x,right_hand_y,right_hand_z,right_hand_euler_x,right_hand_euler_y,right_hand_euler_z," +
+                "left_sword_x,left_sword_y,left_sword_z," +
+                "right_sword_x,right_sword_y,right_sword_z," +
+                "head_x,head_y,head_z,head_euler_x,head_euler_y,head_euler_z");
+
+            /*
+             * In Unity the x,y,z is orientated like the monitor.
+             * x..left-right
+             * y..up-down
+             * z..depth into the monitor
+             * We want x,y to be the earth and z the height.
+             */
+
+            foreach (var frame in frames)
+            {
+                writer.Write(frame.time.ToString("O"));
+                writer.Write(',');
+
+                writer.Write(frame.leftHandPosition[2].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftHandPosition[0].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftHandPosition[1].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.leftHandEuler[2].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftHandEuler[0].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftHandEuler[1].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.rightHandPosition[2].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightHandPosition[0].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightHandPosition[1].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.rightHandEuler[2].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightHandEuler[0].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightHandEuler[1].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.leftSwordPosition[2].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftSwordPosition[0].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.leftSwordPosition[1].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.rightSwordPosition[2].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightSwordPosition[0].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.rightSwordPosition[1].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.headPosition[2].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.headPosition[0].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.headPosition[1].ToString("F3", CultureInfo.InvariantCulture));
+                writer.Write(',');
+
+                writer.Write(frame.headEuler[2].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.headEuler[0].ToString("F1", CultureInfo.InvariantCulture));
+                writer.Write(',');
+                writer.Write(frame.headEuler[1].ToString("F1", CultureInfo.InvariantCulture));
+                writer.WriteLine();
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger, OVRInput.Controller.Touch) || OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger, OVRInput.Controller.Touch))
+        if (OVRInput.GetDown(OVRInput.Button.Four, OVRInput.Controller.Touch))
         {
-            Debug.Log("Started recording");
-            recording = true;
-            recordingText.SetActive(true);
-            frames.Clear();
-            leftTrail.positionCount = 0;
-            rightTrail.positionCount = 0;
-            trailLengthCurrent = trailLength; // reset to the default traillength
+            StartRecording();
         }
 
         if (recording && OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.Touch))
         {
-            Debug.Log("Stopped recording");
-            recording = false;
-            recordingText.SetActive(false);
-            playbackFrame = 10.0f;
-
-            using (var writer = new StreamWriter(File.OpenWrite(Application.persistentDataPath + "/log-" + DateTime.Now.ToString("s") + ".csv")))
-            {
-                writer.WriteLine("timestamp," +
-                    "leftHandX,leftHandY,leftHandZ,leftHandEulerX,leftHandEulerY,leftHandEulerZ," +
-                    "rightHandX,rightHandY,rightHandZ,rightHandEulerX,rightHandEulerY,rightHandEulerZ," +
-                    "leftSwordX,leftSwordY,leftSwordZ," +
-                    "rightSwordX,rightSwordY,rightSwordZ," +
-                    "headX,headY,headZ,headEulerX,headEulerY,headEulerZ");
-
-                /*
-                 * In Unity the x,y,z is orientated like the monitor.
-                 * x..left-right
-                 * y..up-down
-                 * z..depth into the monitor
-                 * We want x,y to be the earth and z the height.
-                 */
-
-                foreach (var frame in frames)
-                {
-                    writer.Write(frame.time.ToString("O"));
-                    writer.Write(',');
-
-                    writer.Write(frame.leftHandPosition[2].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftHandPosition[0].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftHandPosition[1].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.leftHandEuler[2].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftHandEuler[0].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftHandEuler[1].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.rightHandPosition[2].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightHandPosition[0].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightHandPosition[1].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.rightHandEuler[2].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightHandEuler[0].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightHandEuler[1].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.leftSwordPosition[2].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftSwordPosition[0].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.leftSwordPosition[1].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.rightSwordPosition[2].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightSwordPosition[0].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.rightSwordPosition[1].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.headPosition[2].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.headPosition[0].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.headPosition[1].ToString("F3", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-
-                    writer.Write(frame.headEuler[2].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.headEuler[0].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.Write(',');
-                    writer.Write(frame.headEuler[1].ToString("F1", CultureInfo.InvariantCulture));
-                    writer.WriteLine();
-                }
-            }
+            StopRecording();
         }
 
         if (!recording)
